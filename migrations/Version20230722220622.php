ccddcba78adea866011e0d4de632db3232883427
Vasilevds <dmitry.v@vezubr.ru>
2023-07-23 01:07:53 +0300

    create Coupon entity

---
+++ migrations/Version20230722220622.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230722220622 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE coupon_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE coupon (id INT NOT NULL, code VARCHAR(255) NOT NULL, type VARCHAR(20) NOT NULL, value DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_64BF3F028CDE5729 ON coupon (type)');
+        $this->addSql('CREATE INDEX code_idx ON coupon (code)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP SEQUENCE coupon_id_seq CASCADE');
+        $this->addSql('DROP TABLE coupon');
+    }
+}
